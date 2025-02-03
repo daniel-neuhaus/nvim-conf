7f539c34ab7b9e7efcd474761e6ba58abfd92e52
Daniel Neuhaus <mail@daniel-neuhaus.eu>
2025-02-03 19:10:08 +0100

    chore: open neo-tree on startup when not providing buffer to start with

--- init.lua
+++ init.lua
@@ -11,3 +11,11 @@ vim.keymap.set("i", "jj", "<Esc>", { silent = true })
 
 require("config.lazy")
 
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("neo-tree.command").execute({ toggle = true, dir = vim.fn.getcwd() })
+    end
+  end
+})
+
