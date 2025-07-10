7e08d82528aed23ac64546f5df5db8c65941f189
Daniel Schäfer <mail@daniel-neuhaus.eu>
2025-07-10 09:50:13 +0200

    minor updates like bar contents

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,9 +4,16 @@ return {
   config = function()
     require('lualine').setup({
       options = {
-        theme = 'solarized_dark'
+        theme = 'onedark'
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = { },
+        lualine_z = { 'location' }
       }
     })
   end
 }
-
